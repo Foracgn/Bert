285770f00fa608d671297ac1e2e50d906601ffc5
foracgn <729871233@qq.com>
2022-11-19 16:30:16 +0800

    预测返回修改

---
+++ predict.sh
@@ -0,0 +1,13 @@
+python run_race.py \
+--data_dir=../ \
+--bert_model=../bert-base-chinese \
+--output_dir=../large_models \
+--max_seq_length=320 \
+--do_eval --do_lower_case \
+--do_predict \
+--train_batch_size=32 \
+--eval_batch_size=1 \
+--learning_rate=1e-5 \
+--num_train_epochs=1 \
+--gradient_accumulation_steps=8 \
+--loss_scale=128
